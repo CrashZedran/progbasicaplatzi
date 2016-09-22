01cb5d3b1a507b6ecdcb28a18462aa095d1af089
Andres F Robles <crashzedran@gmail.com>
2016-09-22 07:06:03 -0500

    creando la funcion createMalla

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,22 +1,39 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d"); // creamos lienzo en 2d
 var lineas = 30;
-var yi, xf;
 
-for(var l = 0; l < lineas; l++)
+// funcion que maneja la creacion de mallas
+function createMalla(orientacion)
 {
-	yi = 10 * l;
-	xf = 10 * (l + 1);
-	createLine("#AAF", 0, yi, xf, 300);
+	var lineas = 30;
+	var xi, yi, xf, yf;
+
+	for(var l = 0; l < lineas; l++)
+	{
+		if(orientacion == "izquierda"){
+			yi = 10 * l;
+			xf = 10 * (l + 1);
+			createLine("#AAF", 0, yi, xf, 300);
+		}
+		else if(orientacion == "derecha")
+		{
+			xi = 290 - l * 10;
+  			yf = 300 - l * 10;
+  			createLine("#AAF", xi, 0, 300, yf);
+		}
+	}
 }
 
+createMalla("izquierda");
 // agregar lineas a los bordes izquierdo y inferior
 createLine("#F05", 1, 1, 1, 300);
 createLine("#F05", 1, 299, 299, 299);
 
-//createLine("red",100,100,200,200);
-//createLine("blue",50,20,100,200);
+createMalla("derecha");
+createLine("#F05", 299, 1, 299, 299);
+createLine("#F05", 1, 1, 299, 1);
 
+// funcion que maneja la creacion de lineas
 function createLine(color,xInicial, yInicial, xFinal, yFinal)
 {
 	lienzo.beginPath(); // empezar a dibujar un camino
